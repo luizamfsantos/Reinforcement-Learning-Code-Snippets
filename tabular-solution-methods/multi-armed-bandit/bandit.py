d710d3248c414b0b9c6f7587f9d0a6c2a003c921
luizamfsantos <luiza.marques_@hotmail.com>
2024-06-15 23:03:45 -0300

    feat(class): banditenv class to start up the rest of the environments

---
+++ tabular-solution-methods/multi-armed-bandit/bandit.py
@@ -0,0 +1,15 @@
+import numpy as np
+from gym import Env, spaces
+from gym.spaces import Discrete
+from gym.utils import seeding
+
+class BanditEnv(Env):
+    def __init__(self, n_bandits=10):
+        self.n_bandits = n_bandits
+        self._seed = self._seed()
+        self.action_space = Discrete(n_bandits, seed = self._seed, start = 1)
+        self.observation_space = Discrete(1)
+
+    def _seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return seed
