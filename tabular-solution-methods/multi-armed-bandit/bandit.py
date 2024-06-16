21e08de99d7158578f7e9a542ad3bb8294db5ca2
luizamfsantos <luiza.marques_@hotmail.com>
2024-06-16 00:11:43 -0300

    feat: add value functions and update function

--- tabular-solution-methods/multi-armed-bandit/bandit.py
+++ tabular-solution-methods/multi-armed-bandit/bandit.py
@@ -1,6 +1,6 @@
 import numpy as np
 from gym import Env, spaces
-from gym.spaces import Discrete
+from gym.spaces import Discrete, Box
 from gym.utils import seeding
 
 class BanditEnv(Env):
@@ -8,8 +8,10 @@ class BanditEnv(Env):
     def __init__(self, n_bandits=10):
         self.n_bandits = n_bandits
         self._seed = self._seed()
-        self.action_space = Discrete(n_bandits, seed = self._seed, start = 1) # actions are bandit indices
+        self.action_space = Discrete(n_bandits, seed = self._seed) # actions are bandit indices
         self.observation_space = Discrete(1) # single state
+        self.Q = Box(low = 0, high = 1, shape = (n_bandits,), dtype = np.float32) # estimated action values
+        self.N = Box(low = 0, high = 1, shape = (n_bandits,), dtype = np.float32) # action counts for each bandit
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -20,3 +22,6 @@ class BanditEnv(Env):
 
     def step(self, action):
         pass
+
+    def update_value_estimates(self, action, reward):
+        self.Q[action] += 1 / self.N[action] * (reward - self.Q[action])
