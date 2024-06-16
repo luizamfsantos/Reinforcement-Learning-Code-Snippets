31485625eaa6c5f4c76415805940534b9d54c34c
luizamfsantos <luiza.marques_@hotmail.com>
2024-06-15 23:22:20 -0300

    docs: add comment on action space and observation space

--- tabular-solution-methods/multi-armed-bandit/bandit.py
+++ tabular-solution-methods/multi-armed-bandit/bandit.py
@@ -7,8 +7,8 @@ class BanditEnv(Env):
     def __init__(self, n_bandits=10):
         self.n_bandits = n_bandits
         self._seed = self._seed()
-        self.action_space = Discrete(n_bandits, seed = self._seed, start = 1)
-        self.observation_space = Discrete(1)
+        self.action_space = Discrete(n_bandits, seed = self._seed, start = 1) # actions are bandit indices
+        self.observation_space = Discrete(1) # single state
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
